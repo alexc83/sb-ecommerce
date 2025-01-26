846f10382f6b8faeb5c335984737ce8273efc405
alexc83 <alex.cruce@gmail.com>
2025-01-26 02:47:04 -0600

    updates for trying on Codespaces

---
+++ src/main/java/com/ccruce/ecom/project/model/Category.java
@@ -0,0 +1,28 @@
+package com.ccruce.ecom.project.model;
+
+public class Category {
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    private Long categoryId;
+    private String categoryName;
+}
