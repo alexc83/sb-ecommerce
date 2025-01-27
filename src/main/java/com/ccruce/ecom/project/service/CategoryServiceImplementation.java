40d048b0b1262c10cab323ebe1a96ca8a723bb98
Alex Cruce <33612197+alexc83@users.noreply.github.com>
2025-01-27 09:52:23 +0000

    Finished initail Category code

--- src/main/java/com/ccruce/ecom/project/service/CategoryServiceImplementation.java
+++ src/main/java/com/ccruce/ecom/project/service/CategoryServiceImplementation.java
@@ -2,6 +2,7 @@ package com.ccruce.ecom.project.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -38,6 +39,21 @@ public class CategoryServiceImplementation implements CategoryService{
         
         return "Category with category ID: " + categoryId + " deleted successfully";
     
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream()
+            .filter(c -> c.getCategoryId().equals(categoryId))
+            .findFirst();
+        
+            if (optionalCategory.isPresent()) {
+                Category existingCategory = optionalCategory.get();
+                existingCategory.setCategoryName(category.getCategoryName());
+                return existingCategory;
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
+            }
     }
     
 }
