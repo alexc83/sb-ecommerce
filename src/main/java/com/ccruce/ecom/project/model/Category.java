1a358b5054a3a8cc60a17b9a53b44057c0366c1e
alexc83 <alex.cruce@gmail.com>
2025-01-27 16:08:49 -0600

    additional H2 JPA settings

--- src/main/java/com/ccruce/ecom/project/model/Category.java
+++ src/main/java/com/ccruce/ecom/project/model/Category.java
@@ -1,12 +1,21 @@
 package com.ccruce.ecom.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
 
-    private String categoryName;
+    @Id
     private Long categoryId;
+    private String categoryName;
 
     public Category(String categoryName) {
         this.categoryName = categoryName;
+    }
+
+    public Category() {
+
     }
 
     public Long getCategoryId() {
